99c8fd5c2958156402b1cf5615dbdc5fcb9c70a7
Juan Pablo Gutiérrez Restrepo <jgutierrezre@unal.edu.co>
2023-02-25 23:02:11 -0500

    Imported Laser Effects, ported to new version.

---
+++ Assets/Scripts/RaycastGun.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class RaycastGun : MonoBehaviour
+{
+    public Camera playerCamera;
+    public float gunRange = 50f;
+    public float fireRate = 0.2f;
+    public float laserDuration = 10f;
+
+    LineRenderer laserLine;
+    Transform laserOrigin;
+    float fireTimer;
+
+    void Awake()
+    {
+        laserLine = GetComponent<LineRenderer>();
+        laserOrigin = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        fireTimer += Time.deltaTime;
+        if (Input.GetButtonDown("Fire1") && fireTimer > fireRate)
+        {
+            fireTimer = 0;
+            laserLine.SetPosition(0, laserOrigin.position);
+            Vector3 rayOrigin = playerCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hit;
+            if (Physics.Raycast(rayOrigin, playerCamera.transform.forward, out hit, gunRange))
+            {
+                laserLine.SetPosition(1, hit.point);
+                Destroy(hit.transform.gameObject);
+            }
+            else
+            {
+                laserLine.SetPosition(1, rayOrigin + (playerCamera.transform.forward * gunRange));
+            }
+            StartCoroutine(ShootLaser());
+        }
+    }
+
+    IEnumerator ShootLaser()
+    {
+        laserLine.enabled = true;
+        yield return new WaitForSeconds(laserDuration);
+        laserLine.enabled = false;
+    }
+}
